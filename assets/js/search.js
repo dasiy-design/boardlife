d86e264d54df52d84c32a8b7ad1d7520f2fd51da
eungyo0104 <102598754+eungyo0104@users.noreply.github.com>
2025-06-23 16:07:27 +0900

    0623 은교
    search.js완성

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,26 +1,11 @@
 const tabSwiper = document.querySelector('.search-tab');
-const tabBtn = tabSwiper.querySelectorAll('.tab-button');
-// const tabBtnClose = tabBtn.querySelector('.xi-close');
-
-
-// tabBtnClose.addEventListener('click', function(){
-  
-//   console.log(tabBtnClose);
-//   const pe = tabBtn.parentElement;
-//   console.log(pe);
-//   pe.remove();
-// });
-
-const delItem = (event) => {
-  const target = event.target.parentElement;
-  console.log(target);
-  target.remove();    
-};
-
-// tabBtnClose.addEventListener('click', delItem);
-
-
-
-tabBtn.forEach(button => {
-  button.addEventListener('click', delItem);
+const tabBtnClose = document.querySelectorAll('.xi-close');
+tabBtnClose.forEach((btnClose) => {
+  btnClose.addEventListener('click', () => {
+    const tabButton = btnClose.closest('.tab-button') || btnClose.parentElement.closest('.tab-button');
+    
+    if (tabButton) {
+      tabButton.classList.add('close');
+    }
+  });
 });

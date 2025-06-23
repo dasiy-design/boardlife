cd4e6e92128322af32d1dfe424948e60d4fdb26e
eungyo0104 <102598754+eungyo0104@users.noreply.github.com>
2025-06-23 14:00:18 +0900

    0623
    search js

---
+++ assets/js/search.js
@@ -0,0 +1,7 @@
+let tabSwiper = document.getElementById('.search-tab');
+let tabBtn = document.getElementById('.tab-button');
+let tabBtnClose = document.getElementById('.xi-close');
+
+tabBtnClose.addEventListener('click', function(){
+  alert('addEventListener');
+});
